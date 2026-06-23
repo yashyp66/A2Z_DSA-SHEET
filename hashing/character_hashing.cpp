028a82d68f671101fc804f8c353c8ecc2073dc74
Yash Patel <py556530@gmail.com>
2026-06-23 11:49:34 +0530

    Add character_hashing.cpp for character frequency count
    Implement character hashing to count occurrences of each character in a string.

---
+++ hashing/character_hashing.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    int hash[256]={0};
+    for(int i=0;i<s.size();i++){
+        hash[s[i]]++;
+    }
+    int q;
+    cin>>q;
+    while(q--){
+        char c;
+        cin>>c;
+        cout<<hash[c]<<endl;
+    }
+    return 0;
+}
