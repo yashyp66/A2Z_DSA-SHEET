8ab3866b99c4aa74ba371bb59755d4408eda10fd
Yash Patel <py556530@gmail.com>
2026-06-26 17:15:04 +0530

    Refactor merge sort and update input handling
    Refactored merge sort implementation to improve readability and efficiency. Updated input method to accept dynamic array size.

--- Sorting/merge_sort.cpp
+++ Sorting/merge_sort.cpp
@@ -1,54 +1,52 @@
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h>
 using namespace std;
-
-void merge(vector<int>& arr, int left, int mid, int right) {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-
-    vector<int> L(n1), R(n2);
-
-    for (int i = 0; i < n1; i++)
-        L[i] = arr[left + i];
-
-    for (int j = 0; j < n2; j++)
-        R[j] = arr[mid + 1 + j];
-
-    int i = 0, j = 0, k = left;
-
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j])
-            arr[k++] = L[i++];
-        else
-            arr[k++] = R[j++];
+void merge(vector<int>&arr,int low,int mid,int high){
+    vector<int> temp;
+    int left=low;
+    int right=mid+1;
+    while(left<=mid&&right<=high){
+        if(arr[left]<=arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else{
+            temp.push_back(arr[right]);
+            right++;
+        }
     }
-
-    while (i < n1)
-        arr[k++] = L[i++];
-
-    while (j < n2)
-        arr[k++] = R[j++];
-}
-
-void mergeSort(vector<int>& arr, int left, int right) {
-    if (left < right) {
-        int mid = left + (right - left) / 2;
-
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-
-        merge(arr, left, mid, right);
+    while(left<=mid){
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while(right<=high){
+        temp.push_back(arr[right]);
+        right++;
+    }
+    for(int i=low;i<=high;i++){
+        arr[i]= temp[i-low];
     }
 }
+void mergesort(vector<int> &arr,int low,int high){
+    if(low>=high){
+        return;
+    }
+    int mid=(low+high)/2;
+    mergesort(arr,low,mid);
+    mergesort(arr,mid+1,high);
+    merge(arr,low,mid,high);
+   
+}
 
-int main() {
-    vector<int> arr = {38, 27, 43, 3, 9, 82, 10};
-
-    mergeSort(arr, 0, arr.size() - 1);
-
-    cout << "Sorted array: ";
-    for (int x : arr)
-        cout << x << " ";
-
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+       cin>>arr[i];
+    }
+    mergesort(arr,0,n-1);
+     for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
