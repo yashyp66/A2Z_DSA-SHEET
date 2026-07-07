cfd0e1942498a1c5bd82db38716a967257ae3ef6
Yash Patel <py556530@gmail.com>
2026-07-07 22:34:14 +0530

    Create linear_search.cpp

---
+++ Arrays/linear_search.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int linearSearch(int arr[], int n, int key) {
+    for (int i = 0; i < n; i++)
+        if (arr[i] == key)
+            return i;
+    return -1;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int key;
+    cin >> key;
+
+    int index = linearSearch(arr, n, key);
+
+    if (index != -1)
+        cout << index;
+    else
+        cout << -1;
+
+    return 0;
+}
