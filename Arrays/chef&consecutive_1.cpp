6bab57d5539d1f5d9163f2313211bb46878c163c
Yash Patel <py556530@gmail.com>
2026-07-12 21:18:39 +0530

    Create chef&consecutive_1.cpp

---
+++ Arrays/chef&consecutive_1.cpp
@@ -0,0 +1,15 @@
+int findMaxConsecutiveOnes(vector<int>& nums) {
+    int count = 0;
+    int maxi = 0;
+
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] == 1) {
+            count++;
+            maxi = max(maxi, count);
+        } else {
+            count = 0;
+        }
+    }
+
+    return maxi;
+}
