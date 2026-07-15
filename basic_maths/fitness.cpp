9e3346badc5454551ca4c7ff7ed3a4b3ae0cdd71
Yash Patel <py556530@gmail.com>
2026-07-15 21:43:58 +0530

    Create fitness.cpp

---
+++ basic_maths/fitness.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        // your code goes here
+        cout<<x*10<<"\n";
+    }
+}
