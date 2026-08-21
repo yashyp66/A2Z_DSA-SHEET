e42b41d956ded29e2a74266e9dd101763f42ef1d
Yash Patel <py556530@gmail.com>
2026-08-21 22:56:27 +0530

    Create count_inversions_brute.cpp

---
+++ Arrays/count_inversions_brute.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int cnt=0;
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(a[i]>a[j]){
+                cnt++;
+            }
+        }
+    }
+    cout<<cnt;
+    return 0;
+}
